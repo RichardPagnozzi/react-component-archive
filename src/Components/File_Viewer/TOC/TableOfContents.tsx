a7113c845d2b6fa657e7c964ec8acc6af24b2c55
Rich Pagnozzi <richpagnozzi@gmail.com>
2021-12-13 16:57:50 -0800

    * Table of Contents is functional using a <div> in lieu of <textarea>

--- src/Components/File_Viewer/TOC/TableOfContents.tsx
+++ src/Components/File_Viewer/TOC/TableOfContents.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ListSubheader, List, ListItemButton, ListItemIcon, ListItemText, Collapse} from '@mui/material/';
+import { ListSubheader, List, ListItemButton, ListItemIcon, ListItemText, Collapse } from '@mui/material/';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import CircleSharpIcon from '@mui/icons-material/CircleSharp';
@@ -9,10 +9,32 @@ import CircleSharpIcon from '@mui/icons-material/CircleSharp';
 export const TableOfContents = () => {
 
     const [openNested, setOpen] = React.useState(false);
+
+
+    const handleCat1Click = () => {
+        var element = document.getElementById("initial-header")
+        element!.scrollIntoView();
+    }
+    const handleCat2Click = () => {
+        var element = document.getElementById("second-header")
+        element!.scrollIntoView();
+    }
+    const handleCat3Click = () => {
+        var element = document.getElementById("third-header")
+        element!.scrollIntoView();
+    }
+    const handleCat4Click = () => {
+        var element = document.getElementById("fourth-header")
+        element!.scrollIntoView();
+    }
+    const handleCat5Click = () => {
+        var element = document.getElementById("fifth-header")
+        element!.scrollIntoView();
+    }
+
     const handleExpansionClick = () => {
         setOpen(!openNested);
     };
-
 
     return (
         <nav style={{ position: "sticky", top: "24px" }}>
@@ -21,42 +43,56 @@ export const TableOfContents = () => {
                 component="nav"
                 aria-labelledby="nested-list-subheader"
                 subheader={
-                    <ListSubheader sx={{bgcolor:"inherit", color:"black"}}component="div" id="nested-list-subheader">
+                    <ListSubheader sx={{ bgcolor: "inherit", color: "black" }} component="div" id="nested-list-subheader">
                         Table of Contents
                     </ListSubheader>
                 }>
 
-                <ListItemButton sx={{color:"gray"}}>
+                <ListItemButton onClick={handleCat1Click} sx={{ color: "gray" }}>
                     <ListItemIcon>
                         <CircleSharpIcon />
                     </ListItemIcon>
                     <ListItemText primary="Category 1" />
                 </ListItemButton>
 
-                <ListItemButton sx={{color:"gray"}}>
+                <ListItemButton onClick={handleCat2Click} sx={{ color: "gray" }}>
                     <ListItemIcon>
                         <CircleSharpIcon />
                     </ListItemIcon>
                     <ListItemText primary="Category 2" />
                 </ListItemButton>
 
-                <ListItemButton onClick={handleExpansionClick} sx={{color:"gray"}}>
+                <ListItemButton onClick={handleExpansionClick} sx={{ color: "gray" }}>
                     <ListItemIcon>
                         <CircleSharpIcon />
                     </ListItemIcon>
-                    <ListItemText primary="Category 3" />
+                    <ListItemText primary="Sub Categories" />
                     {openNested ? <ExpandLess /> : <ExpandMore />}
                 </ListItemButton>
                 <Collapse in={openNested} timeout="auto" unmountOnExit>
                     <List component="div" disablePadding>
-                        <ListItemButton sx={{ pl: 4, color:"gray"}}>
+                        <ListItemButton onClick={handleCat3Click} sx={{ pl: 4, color: "gray" }}>
                             <ListItemIcon>
                                 <CircleSharpIcon />
                             </ListItemIcon>
-                            <ListItemText primary="Sub-Category 1" />
+                            <ListItemText primary="Category 3" />
+                        </ListItemButton>
+
+                        <ListItemButton onClick={handleCat4Click} sx={{ pl: 4, color: "gray" }}>
+                            <ListItemIcon>
+                                <CircleSharpIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Category 4" />
                         </ListItemButton>
                     </List>
                 </Collapse>
+
+                <ListItemButton onClick={handleCat5Click} sx={{ color: "gray" }}>
+                    <ListItemIcon>
+                        <CircleSharpIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Category 5" />
+                </ListItemButton>
 
             </List>
         </nav>
--- src/Components/File_Viewer/TOC/TableOfContentsDemo.tsx
+++ src/Components/File_Viewer/TOC/TableOfContentsDemo.tsx
@@ -6,16 +6,44 @@ import { CssBaseline } from "@mui/material"
 
 export const TableOfContentsDemo = () => {
 
+    const DummyText =
+        `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+     when an unknown printer took a galley of type and scrambled it to make a type 
+     specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
+     remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
+     containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
 
-
+    let textbox =
+        <nav>
+            <h2 id="initial-header">Category 1</h2>
+            <p>{DummyText}</p>
+            <h2 id="second-header">Category 2</h2>
+            <p>{DummyText}</p>
+            <h2 id="third-header">Category 3</h2>
+            <p>{DummyText}</p>
+            <p>{DummyText}</p>
+            <h2 id="fourth-header">Category 4 </h2>
+            <p>{DummyText}</p>
+            <p>{DummyText}</p>
+            <p>{DummyText}</p>
+            <p>{DummyText}</p>
+            <h2 id="fifth-header">Category 5 </h2>
+            <p>{DummyText}</p>
+            <p>{DummyText}</p>
+        </nav>
+    
     return (
         <div >
             <CssBaseline />
             <div style={{ padding: "10px" }}>
                 <Container sx={{ display: "flex", justifyContent: "space-between" }}>
-                    <div style={{ width: "25%", position: "sticky" }}><TableOfContents /></div>
+                    <div style={{ width: "25%", position: "sticky" }}>
+                        <TableOfContents />
+                    </div>
+
                     <Divider orientation="vertical" flexItem />
-                    <div style={{ width: "75%" }}><TextFileViewer /></div>
+                    <div style={{ width: "75%", overflow:"auto", height:"300px"}}>{textbox}</div>
                 </Container>
             </div>
         </div>
